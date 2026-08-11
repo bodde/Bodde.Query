c834a91018f184f7b66b28d5bb44a1c896664801
bodde <bodde>
2026-08-11 09:47:25 +0200

    Enhance AddQueryServices method to support customizable service registration; introduce IQueryServicesBuilder for flexible configuration

--- Bodde.Query.Core/ServiceCollectionExtensions.cs
+++ Bodde.Query.Core/ServiceCollectionExtensions.cs
@@ -5,29 +5,83 @@ namespace Bodde.Query.Core;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddQueryServices(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
-    {
-        AddQueryServicesCore(services, lifetime);
-        services.Add(new ServiceDescriptor(typeof(IQueryExecutor), typeof(DefaultQueryExecutor), lifetime));
+    public static TServiceCollection AddQueryServices<TServiceCollection>(this TServiceCollection services, Action<IQueryServicesBuilder> builder = null)
+        where TServiceCollection : IServiceCollection
+    {        
+        var queryServicesBuilder = new QueryServicesBuilder();
+        builder?.Invoke(queryServicesBuilder);
+
+        queryServicesBuilder.Build().ForEach(sd => services.Add(sd));
 
         return services;
     }
 
-    public static IServiceCollection AddQueryServices<TQueryExecutor>(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
-        where TQueryExecutor : class, IQueryExecutor
+    public interface IQueryServicesBuilder
     {
-
-        AddQueryServicesCore(services, lifetime);
-        services.AddScoped<IQueryExecutor, TQueryExecutor>();
-
-        return services;
+        public IQueryServicesBuilder WithQueryCriteriaHandler<TQueryCriteriaHandler>() where TQueryCriteriaHandler : class, IQueryCriteriaHandler;
+        public IQueryServicesBuilder WithExpressionBuilder<TExpressionBuilder>() where TExpressionBuilder : class, IExpressionBuilder;
+        public IQueryServicesBuilder WithQueryCriteriaFormatter<TQueryCriteriaFormatter>() where TQueryCriteriaFormatter : class, IQueryCriteriaFormatter;
+        public IQueryServicesBuilder WithQueryCriteriaParser<TQueryCriteriaParser>() where TQueryCriteriaParser : class, IQueryCriteriaParser;
+        public IQueryServicesBuilder WithQueryExecutor<TQueryExecutor>() where TQueryExecutor : class, IQueryExecutor;
+        public IQueryServicesBuilder WithLifetime(ServiceLifetime lifetime);
     }
 
-    private static void AddQueryServicesCore(IServiceCollection services, ServiceLifetime lifetime)
+    private class QueryServicesBuilder : IQueryServicesBuilder
     {
-        services.Add(new ServiceDescriptor(typeof(IQueryCriteriaHandler), typeof(QueryCriteriaHandler), lifetime));
-        services.Add(new ServiceDescriptor(typeof(IExpressionBuilder), typeof(ExpressionBuilder), lifetime));
-        services.Add(new ServiceDescriptor(typeof(IQueryCriteriaFormatter), typeof(ODataFormatter), lifetime));
-        services.Add(new ServiceDescriptor(typeof(IQueryCriteriaParser), typeof(ODataParser), lifetime));
+        ServiceLifetime lifetime = ServiceLifetime.Scoped;
+
+        Type queryCriteriaHandlerType = typeof(QueryCriteriaHandler);
+        Type expressionBuilderType = typeof(ExpressionBuilder);
+        Type queryCriteriaFormatterType = typeof(ODataFormatter);
+        Type queryCriteriaParserType = typeof(ODataParser);
+        Type queryExecutorType = typeof(DefaultQueryExecutor);
+
+        public List<ServiceDescriptor> Build()
+        {
+            return
+            [
+                new(typeof(IQueryCriteriaHandler), queryCriteriaHandlerType, lifetime),
+                new(typeof(IExpressionBuilder), expressionBuilderType, lifetime),
+                new(typeof(IQueryCriteriaFormatter), queryCriteriaFormatterType, lifetime),
+                new(typeof(IQueryCriteriaParser), queryCriteriaParserType, lifetime),
+                new(typeof(IQueryExecutor), queryExecutorType, lifetime)
+            ];
+        }
+
+        public IQueryServicesBuilder WithLifetime(ServiceLifetime lifetime)
+        {
+            this.lifetime = lifetime;
+            return this;
+        }
+
+        IQueryServicesBuilder IQueryServicesBuilder.WithExpressionBuilder<TExpressionBuilder>()
+        {
+            expressionBuilderType = typeof(TExpressionBuilder);
+            return this;
+        }
+
+        IQueryServicesBuilder IQueryServicesBuilder.WithQueryCriteriaFormatter<TQueryCriteriaFormatter>()
+        {
+            queryCriteriaFormatterType = typeof(TQueryCriteriaFormatter);
+            return this;
+        }
+
+        IQueryServicesBuilder IQueryServicesBuilder.WithQueryCriteriaHandler<TQueryCriteriaHandler>()
+        {
+            queryCriteriaHandlerType = typeof(TQueryCriteriaHandler);
+            return this;
+        }
+
+        IQueryServicesBuilder IQueryServicesBuilder.WithQueryCriteriaParser<TQueryCriteriaParser>()
+        {
+            queryCriteriaParserType = typeof(TQueryCriteriaParser);
+            return this;
+        }
+
+        IQueryServicesBuilder IQueryServicesBuilder.WithQueryExecutor<TQueryExecutor>()
+        {
+            queryExecutorType = typeof(TQueryExecutor);
+            return this;
+        }
     }
 }
--- Bodde.Query.Test/ServiceCollection_AddQueryServices.cs
+++ Bodde.Query.Test/ServiceCollection_AddQueryServices.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+using Bodde.Query.Abstractions.Models;
 using Bodde.Query.Abstractions.Services;
 using Bodde.Query.Core;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,49 +8,221 @@ namespace Bodde.Query.Test;
 
 public class ServiceCollection_AddQueryServices
 {
+    private ServiceCollection sut;
+
+    public ServiceCollection_AddQueryServices()
+    {        
+        sut = new ServiceCollection();
+    }
+
 	[Fact]
-    public void With_DefaultQueryExecutor()
+    public void With_DefaultBuilder()
     {
-        var sut = new ServiceCollection();
         sut.AddQueryServices();
 
-        var serviceProvider = sut.BuildServiceProvider();
+        var queryCriteriaHandler = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaHandler));
+        var expressionBuilder = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IExpressionBuilder));
+        var queryCriteriaFormatter = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaFormatter));
+        var queryCriteriaParser = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaParser));
+        var queryExecutor = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryExecutor));
 
-        var queryCriteriaHandler = serviceProvider.GetService<IQueryCriteriaHandler>();
-        var expressionBuilder = serviceProvider.GetService<IExpressionBuilder>();
-        var queryCriteriaFormatter = serviceProvider.GetService<IQueryCriteriaFormatter>();
-        var queryCriteriaParser = serviceProvider.GetService<IQueryCriteriaParser>();
-        var queryExecutor = serviceProvider.GetService<IQueryExecutor>();
+        Assert.NotNull(queryCriteriaHandler);
+        Assert.Equal(ServiceLifetime.Scoped, queryCriteriaHandler.Lifetime);
+        Assert.Equal(typeof(QueryCriteriaHandler), queryCriteriaHandler.ImplementationType);
 
-        Assert.IsType<QueryCriteriaHandler>(queryCriteriaHandler);
-        Assert.IsType<ExpressionBuilder>(expressionBuilder);
-        Assert.IsType<ODataFormatter>(queryCriteriaFormatter);
-        Assert.IsType<ODataParser>(queryCriteriaParser);
-        Assert.IsType<DefaultQueryExecutor>(queryExecutor);
+        Assert.NotNull(expressionBuilder);
+        Assert.Equal(ServiceLifetime.Scoped, expressionBuilder.Lifetime);
+        Assert.Equal(typeof(ExpressionBuilder), expressionBuilder.ImplementationType);
+
+        Assert.NotNull(queryCriteriaFormatter);
+        Assert.Equal(ServiceLifetime.Scoped, queryCriteriaFormatter.Lifetime);
+        Assert.Equal(typeof(ODataFormatter), queryCriteriaFormatter.ImplementationType);
+
+        Assert.NotNull(queryCriteriaParser);
+        Assert.Equal(ServiceLifetime.Scoped, queryCriteriaParser.Lifetime);
+        Assert.Equal(typeof(ODataParser), queryCriteriaParser.ImplementationType);
+
+        Assert.NotNull(queryExecutor);
+        Assert.Equal(ServiceLifetime.Scoped, queryExecutor.Lifetime);
+        Assert.Equal(typeof(DefaultQueryExecutor), queryExecutor.ImplementationType);
+    }
+
+	[Fact]
+    public void WithLifetime_Scoped()
+    {
+        sut.AddQueryServices(builder => builder.WithLifetime(ServiceLifetime.Scoped));
+
+        foreach(var serviceDescriptor in sut)
+        {
+            Assert.Equal(ServiceLifetime.Scoped, serviceDescriptor.Lifetime);
+        }
+    }
+
+	[Fact]
+    public void WithLifetime_Singleton()
+    {
+        sut.AddQueryServices(builder => builder.WithLifetime(ServiceLifetime.Singleton));
+
+        foreach(var serviceDescriptor in sut)
+        {
+            Assert.Equal(ServiceLifetime.Singleton, serviceDescriptor.Lifetime);
+        }
+    }
+
+	[Fact]
+    public void WithLifetime_Transient()
+    {
+        sut.AddQueryServices(builder => builder.WithLifetime(ServiceLifetime.Transient));
+
+        foreach(var serviceDescriptor in sut)
+        {
+            Assert.Equal(ServiceLifetime.Transient, serviceDescriptor.Lifetime);
+        }
     }
 
     [Fact]
-    public void With_CustomQueryExecutor()
+    public void WithQueryCriteriaHandler_Custom()
     {
-        var sut = new ServiceCollection();
-        sut.AddQueryServices<CustomQueryExecutor>();
+        sut.AddQueryServices(builder => builder.WithQueryCriteriaHandler<CustomQueryCriteriaHandler>());
 
-        var serviceProvider = sut.BuildServiceProvider();
+        var actual = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaHandler));
 
-        var queryCriteriaHandler = serviceProvider.GetService<IQueryCriteriaHandler>();
-        var expressionBuilder = serviceProvider.GetService<IExpressionBuilder>();
-        var queryCriteriaFormatter = serviceProvider.GetService<IQueryCriteriaFormatter>();
-        var queryCriteriaParser = serviceProvider.GetService<IQueryCriteriaParser>();
-        var queryExecutor = serviceProvider.GetService<IQueryExecutor>();
-
-        Assert.IsType<QueryCriteriaHandler>(queryCriteriaHandler);
-        Assert.IsType<ExpressionBuilder>(expressionBuilder);
-        Assert.IsType<ODataFormatter>(queryCriteriaFormatter);
-        Assert.IsType<ODataParser>(queryCriteriaParser);
-        Assert.IsType<CustomQueryExecutor>(queryExecutor);
+        Assert.NotNull(actual);
+        Assert.Equal(typeof(CustomQueryCriteriaHandler), actual.ImplementationType);
     }
 
-    class CustomQueryExecutor : IQueryExecutor
+    [Fact]
+    public void WithExpressionBuilder_Custom()
+    {
+        sut.AddQueryServices(builder => builder.WithExpressionBuilder<CustomExpressionBuilder>());
+
+        var actual = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IExpressionBuilder));
+
+        Assert.NotNull(actual);
+        Assert.Equal(typeof(CustomExpressionBuilder), actual.ImplementationType);
+    }
+
+    [Fact]
+    public void WithQueryCriteriaFormatter_Custom()
+    {
+        sut.AddQueryServices(builder => builder.WithQueryCriteriaFormatter<CustomQueryCriteriaFormatter>());
+
+        var actual = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaFormatter));
+
+        Assert.NotNull(actual);
+        Assert.Equal(typeof(CustomQueryCriteriaFormatter), actual.ImplementationType);
+    }
+
+    [Fact]
+    public void WithQueryCriteriaParser_Custom()
+    {
+        sut.AddQueryServices(builder => builder.WithQueryCriteriaParser<CustomQueryCriteriaParser>());
+
+        var actual = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryCriteriaParser));
+
+        Assert.NotNull(actual);
+        Assert.Equal(typeof(CustomQueryCriteriaParser), actual.ImplementationType);
+    }
+
+    [Fact]
+    public void WithQueryExecutor_Custom()
+    {
+        sut.AddQueryServices(builder => builder.WithQueryExecutor<CustomQueryExecutor>());
+
+        var actual = sut.FirstOrDefault(sd => sd.ServiceType == typeof(IQueryExecutor));
+
+        Assert.NotNull(actual);
+        Assert.Equal(typeof(CustomQueryExecutor), actual.ImplementationType);
+    }
+
+    private class CustomQueryCriteriaHandler : IQueryCriteriaHandler
+    {
+        public QueryCriteriaQueryable<T> ApplyCriteria<T>(IQueryable<T> query, QueryCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryCriteriaResult<T>> ToResultAsync<T>(QueryCriteriaQueryable<T> query, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<QueryCriteriaResult<T>> ToResultAsync<T>(IQueryable<T> query, QueryCriteria criteria, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private class CustomExpressionBuilder : IExpressionBuilder
+    {
+        public Expression<Func<T, bool>> CreateFilterExpression<T>(FilterCriteria.FilterExpression filterExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ParameterExpression CreateParameterExpression<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Expression<Func<T, object>> CreatePropertyOrFieldExpressionFromPath<T>(string propertyPath, ParameterExpression? parameter = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private class CustomQueryCriteriaFormatter : IQueryCriteriaFormatter
+    {
+        public string Format(QueryCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatFilter(FilterCriteria filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatOrderBy(OrderByCriteria orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatPaging(PagingCriteria paging)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private class CustomQueryCriteriaParser : IQueryCriteriaParser
+    {
+        public QueryCriteria? Parse(string criteriaString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryCriteria? Parse(QueryCriteriaParameters? queryCriteriaParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FilterCriteria? ParseFilter(string filterString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderByCriteria? ParseOrderBy(string orderByString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PagingCriteria? ParsePaging(string pagingString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    private class CustomQueryExecutor : IQueryExecutor
     {
         public Task<int> CountAsync<T>(IQueryable<T> query, CancellationToken ct = default)
         {
